35e41c7ed6c34164bb56bf533d16e366f0a3465d
Niels Vadot <ninivert1@gmail.com>
2019-09-19 14:25:33 +0200

    wrapper for module and cleanup

--- lyric_shitposter/__main__.py
+++ lyric_shitposter/__main__.py
@@ -1,6 +1,10 @@
-if __name__ == '__main__':
+def main():
 	import logging
 	logging.basicConfig(format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s', level=logging.INFO)
 
 	from .bot import bot
 	bot()
+
+
+if __name__ == '__main__':
+	main()
